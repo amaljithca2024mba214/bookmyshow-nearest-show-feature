8b3d17601546f8959d72a2603bcc0b36aa9658f7
amaljithca2024mba214 <mba24amaljithca@iimsambalpur.ac.in>
2025-12-25 03:08:09 +0530

    Add src/index.js React entry point

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BookMyShowPrototype from './BookMyShowPrototype';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BookMyShowPrototype />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
